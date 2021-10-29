308b6a668f07559103a2d6628769dbfb197ed0c0
Risang <akurisang12@gmail.com>
2021-10-29 10:53:23 +0700

    test login

--- cypress/integration/test_login.js
+++ cypress/integration/test_login.js
@@ -1,8 +1,15 @@
 describe('Log In Scenario - standard_user/secret_sauce', function(){
     it('Log in', function (){
-        cy.visit('https://www.saucedemo.com/')
+        /*cy.visit('https://www.saucedemo.com/')
             cy.get('#user-name').type('standard_user')
             cy.get('#password').type('secret_sauce')
-            cy.get('#login-button').click()
+            cy.get('#login-button').click()*/
+
+            cy.visit('https://www.saucedemo.com/', {
+                auth: {
+                    username: 'standard_user',
+                    password: 'secret_sauce',
+                },
+            })
     })
 })
