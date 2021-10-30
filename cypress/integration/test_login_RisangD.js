3f2a77d6b28f0498af3945cb0bb1087503ec4827
Risang <akurisang12@gmail.com>
2021-10-30 19:01:31 +0700

    test login with new code attempt failed

--- cypress/integration/test_login_RisangD.js
+++ cypress/integration/test_login_RisangD.js
@@ -1,12 +1,36 @@
+describe('Login Tests', function () {
+    it('Successfull login', function () {
+        cy.visit('https://www.saucedemo.com/')
+        /*
+        cy.get(':nth-child(1) > .form-control')
+        .type('test@test.com')
 
-describe('Login Test 1', () => {
+        cy.get(':nth-child(2) > .form-control')
+        .type('test')
+
+        cy.get('.btn')
+        .click()
+
+        cy.url()
+        .should('contain', 'http://localhost:4100/')
+        cy.get(':nth-child(4) > .nav-link')
+        .should('have.attr', 'href', '/@test')
+        cy.get(':nth-child(3) > .nav-link')
+        .should('have.attr', 'href', '/settings')
+        cy.get('.container > .nav > :nth-child(2) > .nav-link')
+        .should('have.attr', 'href', '/editor')*/
+    })
+})
+
+
+/*describe('Login Test 1', () => {
     it('Should be able to login', () => {
       cy.visit('https://www.saucedemo.com/')
       cy.get('#user-name').should('be.visible').type('standard_user')
       cy.get('#password').should('be.visible').type('secret_sauce')
       cy.get('#login-button').click()
     })
-  })
+  })*/
 
 /*
 describe('Login Test 1', function () {
