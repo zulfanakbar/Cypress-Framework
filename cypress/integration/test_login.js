2fa23569a4ab686fa3291676b095a79d4de2343f
Risang <akurisang12@gmail.com>
2021-10-30 09:57:21 +0700

    retest automate access

--- cypress/integration/test_login.js
+++ cypress/integration/test_login.js
@@ -1,16 +1,9 @@
 describe('Log In Scenario - standard_user/secret_sauce', function(){
     it('Log in', function (){
-        /*cy.visit('https://www.saucedemo.com/')
+        cy.visit('https://www.saucedemo.com/')
             cy.get('#user-name').type('standard_user')
             cy.get('#password').type('secret_sauce')
-            cy.get('#login-button').click()*/
-
-            cy.visit('https://www.saucedemo.com/', {
-                auth: {
-                    username: 'standard_user',
-                    password: 'secret_sauce',
-                },
-            })
+            cy.get('#login-button').click()
     })
 })
 /*
