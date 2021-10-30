c422fd7d84d870bf383c62033e50c248b30e32a9
Risang <akurisang12@gmail.com>
2021-10-30 13:22:33 +0700

    tested 5 more login, test failed

--- cypress/integration/test_login_RisangD.js
+++ cypress/integration/test_login_RisangD.js
@@ -6,3 +6,52 @@ describe('Login Test 1', function () {
             cy.get('#login-button').click()
     })
 })
+
+/*
+describe('Log In Scenario - locked_out_user/secret_sauce', function(){
+    it('Log in', function (){
+
+            cy.visit('https://www.saucedemo.com/', {
+                auth: {
+                    username: 'locked_out_user',
+                    password: 'secret_sauce',
+                },
+            })
+    })
+})
+
+describe('Log In Scenario - problem_user/secret_sauce', function(){
+    it('Log in', function (){
+
+            cy.visit('https://www.saucedemo.com/', {
+                auth: {
+                    username: 'problem_user',
+                    password: 'secret_sauce',
+                },
+            })
+    })
+})
+
+describe('Log In Scenario - performance_glitch_user/secret_sauce', function(){
+    it('Log in', function (){
+
+            cy.visit('https://www.saucedemo.com/', {
+                auth: {
+                    username: 'performance_glitch_user',
+                    password: 'secret_sauce',
+                },
+            })
+    })
+})
+
+describe('Log In Scenario - no username/no password', function(){
+    it('Log in', function (){
+
+            cy.visit('https://www.saucedemo.com/', {
+                auth: {
+                    username: '',
+                    password: '',
+                },
+            })
+    })
+})*/
