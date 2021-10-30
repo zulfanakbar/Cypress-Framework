3ba6cc51f13efb4eb762688df5ec6e9f43b9e53f
Risang <akurisang12@gmail.com>
2021-10-30 13:33:16 +0700

    revised code, failed test

--- cypress/integration/test_login_RisangD.js
+++ cypress/integration/test_login_RisangD.js
@@ -1,3 +1,14 @@
+
+describe('Login Test 1', () => {
+    it('Should be able to login', () => {
+      cy.visit('https://www.saucedemo.com/')
+      cy.get('#user-name').should('be.visible').type('standard_user')
+      cy.get('#password').should('be.visible').type('secret_sauce')
+      cy.get('#login-button').click()
+    })
+  })
+
+/*
 describe('Login Test 1', function () {
     it('Should be able to login', function (){
         cy.visit('https://www.saucedemo.com/')
@@ -5,7 +16,7 @@ describe('Login Test 1', function () {
             cy.get('#password').type('secret_sauce')
             cy.get('#login-button').click()
     })
-})
+})*/
 
 /*
 describe('Log In Scenario - locked_out_user/secret_sauce', function(){
